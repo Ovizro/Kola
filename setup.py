eb0948a76a52fd00194fe1ce3f694ececb34e280
ovizro <ovizro@visecy.org>
2025-09-07 17:59:52 +0800

    build(setup): update extension dependencies in setup.py
    - Update the 'depends' parameter for the kola.lexer extension
    - Add dependencies on 'kola/_cutil.h' and 'kola/lex.yy.c'
    - Remove 'include_dirs' parameter as it's no longer needed

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ USE_CYTHON = "USE_CYTHON" in os.environ
 FILE_SUFFIX = ".pyx" if USE_CYTHON else ".c"
 
 extensions = [
-    Extension("kola.lexer", ["kola/lexer" + FILE_SUFFIX, "kola/unicode_handler.c"], include_dirs=["kola"]),
+    Extension("kola.lexer", ["kola/lexer" + FILE_SUFFIX, "kola/unicode_handler.c"], depends=["kola/_cutil.h", "kola/lex.yy.c"]),
     Extension("kola.parser", ["kola/parser" + FILE_SUFFIX]),
     Extension("kola.writer", ["kola/writer" + FILE_SUFFIX])
 ]
