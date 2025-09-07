8c909f06e89c624a8a960ba0ab1871e3f3a4533c
ovizro <ovizro@visecy.org>
2025-09-07 18:11:10 +0800

    fix(kola): define Py_BUILD_CORE when compiling with Python 3.11
    - Add conditional compilation directives to define Py_BUILD_CORE
    - This change is necessary to avoid errors when compiling with Python 3.11
    - The modification is specific to Python version 3.11 and later

--- kola/_cutil.h
+++ kola/_cutil.h
@@ -7,6 +7,9 @@
 #include <Python.h>
 
 #if PY_VERSION_HEX >= 0x030b00a6 && !defined(PYPY_VERSION)
+  #ifndef Py_BUILD_CORE
+    #define Py_BUILD_CORE 1
+  #endif
   #include "internal/pycore_frame.h"
 #endif
 
